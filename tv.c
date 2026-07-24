487b06097a86c9d559ca33084f764c3b6177898e
unnati-31 <unnatiusingh42@gmail.com>
2026-07-24 21:05:58 +0530

    Modified

--- tv.c
+++ tv.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> 
 
+// The 1's represent the TV's border, the 2's represent the actual screen. 
+// The three in the lower right corner are the power buttons.
+//(When 3 is entered the program should output the number of rows and columns.
 int main() {
     int nums[100];
     int length = 0, width = 0;
